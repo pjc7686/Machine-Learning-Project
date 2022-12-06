a47a0d9b4f7ba39d13535c2f1d8623959d87b900
Ben Jordan <bej9038@rit.edu>
2022-12-05 22:07:25 -0500

    Reformatting of training and test sets

--- graphsage/model.py
+++ graphsage/model.py
@@ -17,25 +17,22 @@ from graphsage.aggregators import RandomAggregator
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # seed
-RANDOM_SEED = 1 # G7
+RANDOM_SEED = 1  # G7
 # embedding dimension
-EMBED_DIM = 128 # G7
+EMBED_DIM = 128  # G7
 # epsilon
-EPSILON = 0.1 # G7
+EPSILON = 0.1  # G7
 # batches
-BATCHES = 1000 #G7
-# Epochs
-EPOCHS = 1000
-BATCHES = 200 # G7
+BATCHES = 200  # G7
+# epochs
+EPOCHS = 10  # G7
 # learning rate
 LRATE = .7  # G7
 
-# number of nodes in the cora data
-NUM_NODES_CORA = 2708 # G7
-# number of features in the cora embeddings
-NUM_FEATS_CORA = 1433 # G7
-NUM_SAMPLES_CORA_LAYER1 = 5 # G7
-NUM_SAMPLES_CORA_LAYER2 = 5 # G7
+NUM_NODES_CORA = 2708  # G7
+NUM_FEATS_CORA = 1433  # G7
+NUM_SAMPLES_CORA_LAYER1 = 5  # G7
+NUM_SAMPLES_CORA_LAYER2 = 5  # G7
 
 """
 Simple supervised GraphSAGE model as well as examples running the model
@@ -98,73 +95,40 @@ def run_cora():
     enc2 = Encoder(lambda nodes: enc1(nodes).t(), enc1.embed_dim, EMBED_DIM, adj_lists, agg2, NUM_SAMPLES_CORA_LAYER2, base_model=enc1, gcn=True, cuda=False)
 
     graphsage = SupervisedGraphSage(7, enc2)
+    optimizer = torch.optim.SGD(filter(lambda p: p.requires_grad, graphsage.parameters()), lr=0.7)
     # graphsage.cuda()
     # graphsage.to(device)
 
-    ############# G7
-    # times = []
-
-    # for epoch in range(EPOCHS):
-    #     rand_indices = np.random.permutation(NUM_NODES_CORA)
-    #     test = rand_indices[:1000]
-    #     val = rand_indices[1000:1500]
-    #     train = list(rand_indices[1500:])
-
-
-        # rand_indices = np.random.permutation(NUM_NODES_CORA)
-        # test = rand_indices[:1000]
-        # val = rand_indices[1000:1500]
-        # train = list(rand_indices[1500:])
-
-        # optimizer = torch.optim.SGD(filter(lambda p: p.requires_grad, graphsage.parameters()), lr=0.7)
-
-        # times = []
-
-        # for batch in range(BATCHES):
-        #     batch_nodes = train[:256]
-        #     random.shuffle(train)
-        #     start_time = time.time()
-
-        #     # RANDOM AGGREGATOR #G7
-        #     #chooseAggregator(enc1, enc2, features)
-
-        #     optimizer.zero_grad()
-        #     loss = graphsage.loss(batch_nodes, Variable(torch.LongTensor(labels[np.array(batch_nodes)])))
-        #     loss.backward()
-        #     optimizer.step()
-        #     end_time = time.time()
-        #     times.append(end_time - start_time)
-
-    ############# /G7
-
+    times = []
     rand_indices = np.random.permutation(NUM_NODES_CORA)
     test = rand_indices[:1000]
-    val = rand_indices[1000:1500]
-    train = list(rand_indices[1500:])
 
-    optimizer = torch.optim.SGD(filter(lambda p: p.requires_grad, graphsage.parameters()), lr=LRATE)
-    times = []
+    for epoch in range(EPOCHS):  # G7
 
-    for batch in range(BATCHES):
-        batch_nodes = train[:256]
-        random.shuffle(train)
-        start_time = time.time()
+        rand_indices = np.random.permutation(NUM_NODES_CORA)
+        train = list(rand_indices[2000:])
 
-            # RANDOM AGGREGATOR #G7
-            #chooseAggregator(enc1, enc2, features)
+        for batch in range(BATCHES):
+            batch_nodes = train[:256]
+            random.shuffle(train)
+            start_time = time.time()
 
-        optimizer.zero_grad()
-        loss = graphsage.loss(batch_nodes, Variable(torch.LongTensor(labels[np.array(batch_nodes)])))
-        loss.backward()
-        optimizer.step()
-        end_time = time.time()
-        times.append(end_time - start_time)
+            # RANDOM AGGREGATOR
+            #chooseAggregator(enc1, enc2, features) # G7
+
+            optimizer.zero_grad()
+            loss = graphsage.loss(batch_nodes, Variable(torch.LongTensor(labels[np.array(batch_nodes)])))
+            loss.backward()
+            optimizer.step()
+            end_time = time.time()
+            times.append(end_time - start_time)
+
 
     print("Epochs: ", EPOCHS)
     start_output = time.time()
-    val_output = graphsage.forward(val)
+    test_output = graphsage.forward(test)
     end_output = time.time()
-    print("Validation F1:", f1_score(labels[val], val_output.data.numpy().argmax(axis=1), average="micro"))
+    print("Test F1:", f1_score(labels[test], test_output.data.numpy().argmax(axis=1), average="micro"))
     print("Test Time: ", end_output - start_output)
     print("Average batch time:", np.mean(times))
 
