39d957f51a23e410ff14799b2e1b89f43e89882c
Peter J Carbone <pjc7686@cs.rit.edu>
2022-12-07 18:26:31 -0500

    Removed pubmed

--- graphsage/model.py
+++ graphsage/model.py
@@ -136,80 +136,6 @@ def run_cora():
     print("Test Time: ", end_output - start_output)
     print("Average batch time:", np.mean(times))
 
-
-# number of nodes in the pubmed data
-NUM_NODES_PUBMED = 19717  # G7
-# number of features in pubmed embeddings
-NUM_FEATS_PUBMED = 500  # G7
-NUM_SAMPLES_PUBMED_LAYER1 = 10  # G7
-NUM_SAMPLES_PUBMED_LAYER2 = 25  # G7
-
-
-def load_pubmed():
-    feat_data = np.zeros((NUM_NODES_PUBMED, NUM_FEATS_PUBMED))
-    labels = np.empty((NUM_NODES_PUBMED, 1), dtype=np.int64)
-    node_map = {}
-    with open("pubmed-data/Pubmed-Diabetes.NODE.paper.tab") as fp:
-        fp.readline()
-        feat_map = {entry.split(":")[1]:i-1 for i,entry in enumerate(fp.readline().split("\t"))}
-        for i, line in enumerate(fp):
-            info = line.split("\t")
-            node_map[info[0]] = i
-            labels[i] = int(info[1].split("=")[1])-1
-            for word_info in info[2:-1]:
-                word_info = word_info.split("=")
-                feat_data[i][feat_map[word_info[0]]] = float(word_info[1])
-    adj_lists = defaultdict(set)
-    with open("pubmed-data/Pubmed-Diabetes.DIRECTED.cites.tab") as fp:
-        fp.readline()
-        fp.readline()
-        for line in fp:
-            info = line.strip().split("\t")
-            paper1 = node_map[info[1].split(":")[1]]
-            paper2 = node_map[info[-1].split(":")[1]]
-            adj_lists[paper1].add(paper2)
-            adj_lists[paper2].add(paper1)
-    return feat_data, labels, adj_lists
-
-
-def run_pubmed():
-    feat_data, labels, adj_lists = load_pubmed()
-    features = nn.Embedding(NUM_NODES_PUBMED, NUM_FEATS_PUBMED)
-    features.weight = nn.Parameter(torch.FloatTensor(feat_data), requires_grad=False)
-    # features.cuda()
-
-    agg1 = MeanAggregator(features, cuda=True)
-    enc1 = Encoder(features, NUM_FEATS_PUBMED, EMBED_DIM, adj_lists, agg1, NUM_SAMPLES_PUBMED_LAYER1, gcn=True, cuda=False)
-    agg2 = MeanAggregator(lambda nodes : enc1(nodes).t(), cuda=False)
-    enc2 = Encoder(lambda nodes : enc1(nodes).t(), enc1.embed_dim, EMBED_DIM, adj_lists, agg2, NUM_SAMPLES_PUBMED_LAYER2,
-                   base_model=enc1, gcn=True, cuda=False)
-
-    graphsage = SupervisedGraphSage(3, enc2)
-    # graphsage.cuda()
-    rand_indices = np.random.permutation(NUM_NODES_PUBMED)
-    test = rand_indices[:1000]
-    val = rand_indices[1000:1500]
-    train = list(rand_indices[1500:])
-
-    optimizer = torch.optim.SGD(filter(lambda p : p.requires_grad, graphsage.parameters()), lr=LRATE)
-    times = []
-
-    for batch in range(200):
-        batch_nodes = train[:1024]
-        random.shuffle(train)
-        start_time = time.time()
-        optimizer.zero_grad()
-        loss = graphsage.loss(batch_nodes, Variable(torch.LongTensor(labels[np.array(batch_nodes)])))
-        loss.backward()
-        optimizer.step()
-        end_time = time.time()
-        times.append(end_time-start_time)
-
-    val_output = graphsage.forward(val)
-    print("Validation F1:", f1_score(labels[val], val_output.data.numpy().argmax(axis=1), average="micro"))
-    print("Average batch time:", np.mean(times))
-
-
 # G7
 def chooseAggregator(encoder1, encoder2, features):
     if random.random() < EPSILON:
@@ -235,7 +161,6 @@ def init_seeds():
 def main():
     init_seeds()
     run_cora()
-    #run_pubmed()
 
 
 if __name__ == "__main__":
